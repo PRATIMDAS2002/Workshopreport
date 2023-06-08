fb37d0553982f28bb9d389f22aeaf9fd57e0255c
PRATIMDAS2002 <pratimdas2002@gmail.com>
2023-06-08 15:41:37 +0530

    initial commit

---
+++ new3.js
@@ -0,0 +1,9 @@
+let bold = document.createElement("b")
+bold.innerText = "bold text here"
+d.append(bold)
+
+let parahold = document.getElementById("p1")
+let boldText = document.createElement("b")
+boldText.innerText = "this is the contet of parabold"
+parabold.append(boldText)
+
